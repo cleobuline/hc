daac18dbf9f91ccb80612d88fd57d3a8506f9a82
cleobuline <110346262+cleobuline@users.noreply.github.com>
2026-07-25 11:51:00 +0200

    Initial Commit

---
+++ HC/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  HC
+//
+//  Created by Patricia Benedetto on 25/07/2026.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ HC/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  HC
+//
+//  Created by Patricia Benedetto on 25/07/2026.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
